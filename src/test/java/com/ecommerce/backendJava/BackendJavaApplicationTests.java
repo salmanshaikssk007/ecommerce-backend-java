d2b0f8ce9a30f611f645a6aadfb7fdc35a4f749e
salmanshaikssk007 <salmanshaikssk007@gmail.com>
2024-09-26 13:11:01 -0500

    Intial commit

---
+++ src/test/java/com/ecommerce/backendJava/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.backendJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
